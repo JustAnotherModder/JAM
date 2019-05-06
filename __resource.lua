993daf3a427656f4eaccce07c8e3f2812a8105e4
JustAnotherModder <48855422+JustAnotherModder@users.noreply.github.com>
2019-05-06 15:22:07 +0930

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,17 +1,35 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
+	-- Base
 	'JAM_Main.lua',
+	'JAM_Client.lua',
 	'JAM_Utilities.lua',
 
-	'JAM_Commands.lua',
+	-- Commands
+	--'JAM_Commands/JAM_Commands.lua',
+
+	-- Teleport
+	--'JAM_Teleport/JAM_Teleport_Config.lua',
+	--'JAM_Teleport/JAM_Teleport_Client.lua',
+
+	-- Garage
+	--'JAM_Garage/JAM_Garage_Config.lua',
+	--'JAM_Garage/JAM_Garage_Client.lua',	
 }
 
 server_scripts {	
+	-- Base
 	'JAM_Main.lua',
+	'JAM_Server.lua',
 	'JAM_Utilities.lua',
 
+	-- MySQL
 	'@mysql-async/lib/MySQL.lua',
+
+	-- Garage
+	--'JAM_Garage/JAM_Garage_Config.lua',
+	--'JAM_Garage/JAM_Garage_Server.lua',
 }
 
 files {	
