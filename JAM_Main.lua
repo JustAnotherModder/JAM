a44cc1241c3083fae852cafb0c3cc5a614866c1f
JustAnotherModder <48855422+JustAnotherModder@users.noreply.github.com>
2019-05-15 22:49:12 +0930

    Update JAM_Main.lua

--- JAM_Main.lua
+++ JAM_Main.lua
@@ -1,15 +1,16 @@
 JAM = {}
 
-JAM.Testing = false
-
+TriggerEvent('esx:getSharedObject', function(obj) JAM.ESX = obj; ESX = obj; end)
 AddEventHandler('onMySQLReady', function(...) JAM.SQLReady = true; end)
+
+JAM.Testing = false
 
 Citizen.CreateThread(function(...)
 	while not ESX or not JAM.ESX do 
 		Citizen.Wait(0)
 		TriggerEvent('esx:getSharedObject', function(obj) JAM.ESX = obj; ESX = obj; end)
 	end
-	
+
 	if JAM.Testing then 
 		Citizen.Wait(3000)
 		if not JAM.SQLReady then JAM.SQLReady = true; end
